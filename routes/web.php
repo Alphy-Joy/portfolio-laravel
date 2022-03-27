c8e29c28d1044f367486160f32481e3fa731b6ca
alphyjoy <contact.alphyjoy@gmail.com>
2022-03-27 18:57:22 +0530

    Query builder completed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/',[PagesController::class,'index']);
-//Route::get('/', [PagesController::class,'index']);
+Route::get('posts/',[PostsController::class,'index']);
